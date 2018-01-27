3b2e21a85d7627a963313376ad5a6daabd6af80d
Torn <ott.adermann@gmail.com>
2018-01-27 23:01:43 +0200

    smoke turns off

---
+++ Assets/Scripts/FactorySmokeEnabler.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FactorySmokeEnabler : MonoBehaviour {
+	public SmokeProducer smoker;
+	public EnergyConsumer consumer;
+
+	void FixedUpdate() {
+		if (consumer.hasEnergy) {
+			if (!smoker.enabled) {
+				smoker.enabled = true;
+			}
+		} else if (smoker.enabled) {
+			smoker.enabled = false;
+		}
+	}
+}
