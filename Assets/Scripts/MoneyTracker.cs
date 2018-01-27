8e746461412df1bb912d2a27770b30176ac6b193
Torn <ott.adermann@gmail.com>
2018-01-27 17:42:10 +0200

    input fixes, ui start

--- Assets/Scripts/MoneyTracker.cs
+++ Assets/Scripts/MoneyTracker.cs
@@ -1,5 +1,15 @@
 ﻿using UnityEngine;
+using UnityEngine.UI;
 
 public class MoneyTracker : MonoBehaviour {
 	public float money = 0;
+	Text moneyText;
+
+	void Start() {
+		moneyText = GameObject.Find("Money").GetComponent<Text>();
+	}
+
+	void Update() {
+		moneyText.text = "$" + Mathf.RoundToInt(money);
+	}
 }
