c2e7684a2f9653ae00d1bb3882c3b1c2c161a0e7
VillemRootalu <vileem@gmail.com>
2018-01-27 23:39:50 +0200

    Audiomanager and some sfx

---
+++ Assets/Scripts/Audiomanager.cs
@@ -0,0 +1,66 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Audio;
+
+public class Audiomanager : MonoBehaviour {
+
+    public AudioClip BuildWire;
+    public AudioClip BuildWind;
+    public AudioClip BuildNuclear;
+    public AudioClip UISell;
+    public AudioClip UISelect;
+    public AudioClip UIDenied;
+    public AudioClip UIUpgrade;
+
+    public AudioSource Wiresource;
+    public AudioSource Windsource;
+    public AudioSource Nuclearsource;
+    public AudioSource Sellsource;
+    public AudioSource Selectsource;
+    public AudioSource Deniedsource;
+    public AudioSource Upgradesource;
+
+
+
+
+    void Start () {
+        Wiresource.clip = BuildWire;
+        Windsource.clip = BuildWind;
+        Nuclearsource.clip = BuildNuclear;
+        Sellsource.clip = UISell;
+        Selectsource.clip = UISelect;
+        Deniedsource.clip = UIDenied;
+        Upgradesource.clip = UIUpgrade;
+    }
+
+    public void PlayWire()
+
+    {
+        Wiresource.Play();
+    }
+    public void PlayWind() {
+        Windsource.Play();
+    }
+    public void PlayNuclear() {
+        Nuclearsource.Play();
+    }
+    public void PlaySell() {
+        Sellsource.Play();
+    }
+    public void PlaySelect() {
+        Selectsource.Play();
+    }
+    public void PlayDenied() {
+        Deniedsource.Play();
+    }
+    public void PlayUpgrade() {
+        Upgradesource.Play();
+    }
+	// Update is called once per frame
+	void Update () {
+
+        
+       
+    }
+}
