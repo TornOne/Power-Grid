9ef03cce5c055cba33692fc5cdea8b4504b08ca9
Torn <ott.adermann@gmail.com>
2018-01-28 13:02:13 +0200

    map gen pretty

--- Assets/Scripts/Managers/PerlinMapGenerator.cs
+++ Assets/Scripts/Managers/PerlinMapGenerator.cs
@@ -3,9 +3,9 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class PerlinMapGenerator : MonoBehaviour {
-	public List<List<Tile>> GenerateMap(Tile tilePrefab, int width, int height, float waterAmount = 0.3f) {
-		float offsetX = Random.Range(0f, float.MaxValue / 2);
-		float offsetY = Random.Range(0f, float.MaxValue / 2);
+	public List<List<Tile>> GenerateMap(Tile tilePrefab, int width, int height, float waterAmount = 0.4f) {
+		float offsetX = Random.Range(0f, 1024f);
+		float offsetY = Random.Range(0f, 1024f);
 		GameObject tileParent = new GameObject("Tiles");
 
 		List<List<Tile>> grid = new List<List<Tile>>(height);
@@ -13,12 +13,14 @@ public class PerlinMapGenerator : MonoBehaviour {
 		for (int x = 0; x < width; x++) {
 			List<Tile> row = new List<Tile>(width);
 
-			for (int y = 0; y < width; y++) {
+			for (int y = 0; y < height; y++) {
 				Tile tile = Instantiate(tilePrefab, new Vector3(x, 0, y), Quaternion.Euler(90, 0, 0), tileParent.transform);
 				tile.name = "Tile " + x + ", " + y;
 				tile.gridPosition = new Vector2Int(x, y);
 
-				float value = Mathf.PerlinNoise(offsetX + x * 0.03f, offsetY + y * 0.03f);
+				float value = Mathf.PerlinNoise(offsetX + 8f * x / width, offsetY + 8f * y / height) * 0.4f;
+				value += Mathf.PerlinNoise((offsetX + 8f * x / width) * 2.5f, (offsetY + 8f * y / height) * 2.5f) * 0.2f;
+				value += Mathf.PerlinNoise((offsetX + 8f * x / width) * 0.3f, (offsetY + 8f * y / height) * 0.3f) * 0.4f;
 				Debug.Log(x + " " + y + " " + value);
 				if (value > waterAmount) {
 					tile.SetType(Tile.Type.Grass);
